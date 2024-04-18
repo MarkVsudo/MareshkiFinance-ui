8f3ef239030e2459515107be0da6e21b1727fb91
Mark Veskov <mark.veskov@abv.bg>
2024-04-18 15:13:05 +0300

    Designing dashboard page

--- front-end-cleverpine/src/App.tsx
+++ front-end-cleverpine/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
+import DashboardPage from "./components/DashboardPage/DashboardPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- front-end-cleverpine/src/components/LoginPage/LoginPage.tsx
+++ front-end-cleverpine/src/components/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/login.css";
+import "../../styles/auth.css";
 import {
   FormControl,
   FormLabel,
